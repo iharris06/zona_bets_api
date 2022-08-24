df74fab628b3d0fddb1c06beb2d0c19557bc8632
iharris06 <ian.harris4@gmail.com>
2022-08-23 23:21:34 -0700

    Add objects to schema.ex

--- lib/zona_bets_api_web/schema.ex
+++ lib/zona_bets_api_web/schema.ex
@@ -3,6 +3,25 @@ defmodule ZonaBetsApiWeb.Schema do
 
   alias ZonaBetsApiWeb.NewsResolver
 
+  object :transaction do
+    field :id, non_null(:id)
+    field :amount, non_null(:integer)
+    field :bettor_id, non_null(:id)
+    field :match_id, non_null(:id)
+  end
+
+  object :match do
+    field :id, non_null(:id)
+    field :description, non_null(:string)
+    field :result, non_null(:string)
+  end
+
+  object :bettor do
+    field :id, non_null(:string)
+    field :name, non_null(:string)
+    field :bank, non_null(:integer)
+  end
+
   query do
     # this is the query entry point to our app
   end
